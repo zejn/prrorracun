b4d57d261fffcca3d83161e5a454504b7f40cc45
Gasper Zejn <zejn@kiberpipa.org>
2012-10-24 21:29:54 +0200

    New urls.

--- proracun/urls.py
+++ proracun/urls.py
@@ -15,18 +15,18 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^p/$', 'proracun.views.proracun_list'),
+    url(r'^drzava/proracun/$', 'proracun.views.proracun_list'),
     # treemap
-    url(r'^p/(?P<po>(:?prihodki|odhodki))/(?P<leto>\d{4})/(?P<date>\d{4}-\d\d-\d\d)/$', 'proracun.views.treemap', name='proracun_treemap'),
-    url(r'^p/proracun_(?P<po>(:?prihodki|odhodki))_(?P<leto>\d{4})_(?P<date>\d{4}-\d\d-\d\d)\.js$', 'proracun.views.treemap_js', name='proracun_treemap_js'),
+    url(r'^drzava/proracun/(?P<po>(:?prihodki|odhodki))/(?P<leto>\d{4})/(?P<date>\d{4}-\d\d-\d\d)/$', 'proracun.views.treemap', name='proracun_treemap'),
+    url(r'^drzava/proracun/proracun_(?P<po>(:?prihodki|odhodki))_(?P<leto>\d{4})_(?P<date>\d{4}-\d\d-\d\d)\.js$', 'proracun.views.treemap_js', name='proracun_treemap_js'),
     # areachart
-    url(r'^stuff/20letSlovenije/proracun_area_(?P<po>(:?prihodki|odhodki))_(?:(?P<language>(?:sl|en))_)?(?P<sifra>\d+)_(?P<inflacija>...)\.js$', 'proracun.views.areachart_js', name='proracun_areachart_js'),
-    url(r'^stuff/20letSlovenije/(?:(?P<language>en)/)?(?P<po>(:?prihodki|odhodki))/(?P<inflacija>...)/$', 'proracun.views.areachart', name='proracun_areachart'),
+    url(r'^drzava/proracun/proracun_area_(?P<po>(:?prihodki|odhodki))_(?:(?P<language>(?:sl|en))_)?(?P<sifra>\d+)_(?P<inflacija>...)\.js$', 'proracun.views.areachart_js', name='proracun_areachart_js'),
+    url(r'^drzava/proracun/(?:(?P<language>en)/)?(?P<po>(:?prihodki|odhodki))/(?P<inflacija>...)/$', 'proracun.views.areachart', name='proracun_areachart'),
 )
 
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^stuff/20letSlovenije/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        (r'^drzava/proracun/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     )
 
